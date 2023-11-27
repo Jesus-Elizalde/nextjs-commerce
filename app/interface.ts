64c686db70f8db438ce83a86439153ade60a32a3
Jesus Elizalde <elizaldejesus34@gmail.com>
2023-11-27 13:56:14 -0800

    product page init

--- app/interface.ts
+++ app/interface.ts
@@ -6,3 +6,12 @@ export interface simplifiedProduct {
   categoryName: string;
   name: string;
 }
+export interface fullProduct {
+  _id: string;
+  images: any;
+  price: number;
+  slug: string;
+  categoryName: string;
+  name: string;
+  description: string;
+}
